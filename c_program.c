094cbaa8325b51cc206e8b67025440b3c6b01801
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2022-07-24 00:08:25 +0200

    played a little bit with the c program

--- c_program.c
+++ c_program.c
@@ -1,6 +1,31 @@
+
+asm("j _start");
+
+void print_int(int x)
+{
+  asm("li a7, 1");
+  asm("ecall");
+}
+
+void quit(int code)
+{
+  asm("li a7, 93");
+  asm("ecall");
+}
+
+int fib(int n)
+{
+  if(n == 0) return 0;
+  if(n == 1) return 1;
+  return fib(n-1) + fib(n-2);
+}
+
 void _start()
 {
   int x = 0, y;
   x++;
   y = x * 2 + 1;
+  print_int(y);
+  print_int(fib(30));
+  quit(0);
 }
