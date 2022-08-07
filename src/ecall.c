1c96eb603f8accdbdd4efed7b2ec9cca52d705b8
Maxime ANDRÉ <maxime.andre1986@gmail.com>
2022-08-07 22:48:02 +0200

    Added enum of register names

--- src/ecall.c
+++ src/ecall.c
@@ -3,17 +3,18 @@
 #endif
 
 #include "rv32.h"
+#include "ecall.h"
 
 void ecall(RV32 *rv32)
 {
-  switch(rv32->r[17]) { /* switch(a7) */
+  switch(rv32->r[REG_A7]) {
   case 1:
-    printf("%c", rv32->r[10]);
+    printf("%c", rv32->r[REG_A0]);
     break;
   case 2:
-    printf("%d\n", rv32->r[10]);
+    printf("%d\n", rv32->r[REG_A0]);
     break;
-  case 93:
+  case 93: /* exit */
     rv32->halted = 1;
     break;
   default:
